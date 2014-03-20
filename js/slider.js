0afe1f2bce697c387824e29f5acc177324342444
Brian Wald <briancwald@gmail.com>
2014-03-20 13:31:21 -0700

    fix the slider to work with multiple instances, use the drupal "once" method and storage values with LocalStorage

--- js/slider.js
+++ js/slider.js
@@ -1,25 +1,39 @@
 (function ($) {
-  Drupal.behaviors.media_preview_sizer = {
-    attach: function(context, settings) {
+    Drupal.behaviors.media_preview_sizer = {
+        attach: function (context, settings) {
 
-      //add in slider support for image sizing in the library
+            //add in slider support for image sizing in the library
 
-      //find the exposed filters and add the slider inline with them
-       $(".media-browser-wrapper .views-exposed-form .views-exposed-widgets").append("<div class='views-exposed-widget slide-widget'><label>Image Size</label><div id='slide-image'></div></div>")
-      
-       //using a preset image style with a max width of 300, set the minimum and starting value
-       $('#slide-image').slider({
-           value: 200,
-           min: 100,
-           max: 300,
-             step: 2,
+            //find the exposed filters and add the slider inline with them
+            $(".media-browser-wrapper .views-exposed-form .views-exposed-widgets").once('media_preview_sizer').append("<div class='views-exposed-widget slide-widget'><label>Image Size</label><div class='slide-image'></div></div>");
 
-            //when the slider moves, resize the image according to the px amo
-         slide: function(event, ui) {
-            $('#media-browser-library-list li').css('width', ui.value + 'px');
+
+            var valued = (!localStorage.getItem("slideWidth")) ? 200 : localStorage.getItem("slideWidth");
+
+            //set a default CSS size for the list items
+            $('#media-browser-library-list li').css('width', localStorage.getItem('slideWidth') + 'px');
+
+            //using a preset image style with a max width of 300, set the minimum and starting value
+            $('.slide-image').once('media_preview_sizer').slider({
+                value: valued,
+                min: 100,
+                max: 300,
+                step: 2,
+
+                //when the slider moves, resize the image according to the px amount
+                slide: function (event, ui) {
+                    //store value in localstorage
+                    localStorage.setItem('slideWidth', ui.value);
+                    $('#media-browser-library-list li').css('width', localStorage.getItem('slideWidth') + 'px');
+
+
+                }
+
+
+
+
+            });
+
         }
-    });
- 
-    }
-  };
+    };
 }(jQuery));
